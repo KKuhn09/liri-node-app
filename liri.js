a4fce273ec71787a051c344598fdecc3eac6d1d8
Kieran Kuhn <kuhn.kieran@gmail.com>
2017-11-07 22:52:18 -0700

    Added spotify-this-song command

--- liri.js
+++ liri.js
@@ -1,21 +1,25 @@
-//Includes request npm package
-var request = require("request");
-//Include twitter package/keys
-var Twitter = require("twitter");
-//Keep twitterKeys private
-var twitterKeys = require("./keys.js");
+var request = require("request");//Includes request npm package
+var Twitter = require("twitter");//Include twitter package
+var Spotify = require("node-spotify-api");//Include spotify package
+
+var twitterKeys = require("./keys.js");//Keep twitterKeys private
+//Create our Twitter client
 var client = new Twitter({
   consumer_key: twitterKeys.twitterKeys.consumer_key,
   consumer_secret: twitterKeys.twitterKeys.consumer_secret,
   access_token_key: twitterKeys.twitterKeys.access_token_key,
   access_token_secret: twitterKeys.twitterKeys.access_token_secret,
 });
+//Create the Spotify client
+var spotify = new Spotify({
+  id: "7084abeda56d4bcb9b19cf6aa1e640c4",
+  secret: "4911edbea69e4c989dae8b52fbe9581b"
+});
 
+var nodeArgs = process.argv;
 //If movie-this is the third argument on the command line
-if(process.argv[2] === "movie-this"){
+if(nodeArgs[2] === "movie-this"){
 
-	//Stores arguments into array
-	var nodeArgs = process.argv;
 	//Initializes var where we will store the movie name
 	var movieName = "";
 
@@ -33,8 +37,7 @@ if(process.argv[2] === "movie-this"){
 	//Creates a query URL based on movie name we just created
 	var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
 
-	//Using request package, pull the .json response from the 
-	//OMDB API URL we created 
+	//Using request package, pull the .json response from the OMDB API URL we created 
 	request(queryUrl, function(error, response, body) {
 	  	// If the request is successful
 	  	if (!error && response.statusCode === 200) {
@@ -53,7 +56,7 @@ if(process.argv[2] === "movie-this"){
 	});
 }
 
-//If the third argument in command line is my-tweets
+//If my-tweets is the third argument in command line
 if(process.argv[2] === "my-tweets"){
 	//Use the Twitter client to get tweets from my timeline
 	client.get("https://api.twitter.com/1.1/statuses/user_timeline.json", function(error, tweets, response) {
@@ -64,4 +67,32 @@ if(process.argv[2] === "my-tweets"){
   			console.log("Tweet "+(i+1)+": "+tweets[i].text)
   		};  
 	});
+}
+
+//If spotify-this-song is the third argument in command line
+if(process.argv[2] === "spotify-this-song"){
+	var songTitle = "";
+	//Starts a for loop that will patch the song title together with ' 's
+	for (var i = 3; i < nodeArgs.length; i++) {
+		//If there is more than one word in the song title
+		if (i > 3 && i < nodeArgs.length){
+			//Patch together song title
+			songTitle = songTitle + " " + nodeArgs[i];
+		}
+	  	else{
+	   		songTitle += nodeArgs[i];
+	  	}
+	}
+	//
+	spotify
+		.search({ type: 'track', query: songTitle, limit: 1 })
+		.then(function(response){
+			console.log("Artist: "+response.tracks.items[0].album.artists[0].name);
+			console.log("Song Title: "+response.tracks.items[0].name);
+			console.log("Preview Link: "+response.tracks.items[0].external_urls.spotify);
+			console.log("Album: "+response.tracks.items[0].album.name);
+		})
+		.catch(function(err){
+			console.log(err);
+		});
 }
